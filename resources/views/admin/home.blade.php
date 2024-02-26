680e896e3a9717da072c503a5b4a0dc708cc26d1
SajidRhmn <r.mohammadsajid@gmail.com>
2024-02-26 21:10:57 +0600

    added innitial files

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+</head>
+<body>
+   Admin Panel
+</body>
+</html>
+
+</x-app-layout>
